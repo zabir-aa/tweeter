ad63a990b2644868279bab87690af2022c8206be
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-10-24 15:15:49 -0400

    Define a function called loadTweets, which will use jQuery to make a request to /tweets and receive the array of tweets as JSON and delegates the data to drive the renderTweets function, remove the hard-coded tweets object that was added earlier to drive the renderTweets function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,4 @@
 $( document ).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
   
   const createTweetElement = function (tweet) {
     const $tweet = $(`
@@ -58,13 +34,26 @@ $( document ).ready(function() {
       $('#tweet-container').append($tweet);
     }
   };
-  renderTweets(data);
 
   $("#tweet-form").submit(function(event) {
     event.preventDefault();
     const serializedData = $(this).serialize();
     $.post("/tweets", serializedData)
   });
+
+  const loadTweets = $.ajax({
+    url: "/tweets",
+    method: "GET",
+    dataType: "json",
+    success: (tweets) => {
+      renderTweets(tweets);
+    },
+    error: (error) => {
+      console.log(error)
+    }
+  });
+
+  loadTweets();
 
   /*
   const $button = $('.container #footer button');
