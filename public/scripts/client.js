d27e8e4eff519c53ec4cc46e1e8ede9f2016c0e2
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-11-11 23:53:06 -0500

    Add comment, remove unnecessary lines

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
+
+  // Function to escape sensitive scripting characters from input text to prevent XSS attacks.
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
-  const createTweetElement = function(tweet) { //
+  // Creates the HTML body of an individual tweet from the tweet object.
+  const createTweetElement = function(tweet) {  
     const $tweet = $(`
     <article class="tweet">
       <heading>
@@ -32,40 +34,47 @@ $(document).ready(function() {
     `);
     return $tweet;
   };
-  
+  //renderTweets: Loops through all tweet data, Creates individual tweets for each, Stacks tweets by prepending.
   const renderTweets = function(tweets) {
     for (const tweetData of tweets) {
       const $tweet = createTweetElement(tweetData);
       $('#tweet-container').prepend($tweet);
     }
   };
+  // Defaults the hidden property of the error element
   $("#error-element").empty().hide();
   
+  // Defaults the hidden property of the tweet compose box
   $(".new-tweet").hide();
 
+  // Toggles the tweet compose box on clicking the 'create new tweet' button on the Nav bar
   $("#composeNewTweet").click(function() {
     return $('.new-tweet').toggle("slow").focus();
   });
 
+  //Tweet form submit event
   $("#tweet-form").submit(function(event) {
-    event.preventDefault();
-    const serializedData = $(this).serialize();
-    const textInput = $('#tweet-text').val();
-    if (textInput.trim() === "" || textInput === null) {
-      $("#error-element").text("Your post is empty. Are you not humming about anything?").hide().slideDown("slow");
-    } else if (textInput.length > 140) {
-      $("#error-element").text("That's too large to be a tweet! Tweet within 140 characters please").hide().slideDown("slow");
-    } else {
-      $.post("/tweets", serializedData)
-        .then(loadTweets())
-        .then(loadTweets())
-        .then(loadTweets());
-      $("#error-element").empty().hide();
-      $('#tweet-text').val("");
-      $("#charCount").val(140);
+    event.preventDefault(); // stop default form submit to run below conditions
+    const serializedData = $(this).serialize(); // serialize form data to extract text value
+    const textInput = $('#tweet-text').val(); // store text length
+    if (textInput.trim() === "" || textInput === null) { // check for empty tweet
+      $("#error-element").text("Your post is empty. Are you not humming about anything?").hide().slideDown("slow"); // error element to show for empty tweet
+    } else if (textInput.length > 140) { // check for too large tweet
+      $("#error-element").text("That's too large to be a tweet! Tweet within 140 characters please").hide().slideDown("slow"); // error element to show for empty tweet
+    } else { // when conditions are met
+      $.post("/tweets", serializedData) // posts tweet text to /tweets route
+        .then(loadTweets()) // renders the tweets on the page 1 time
+        .then(loadTweets())  // renders the tweets on the page a 2nd time to avoid failure
+        .then(loadTweets());  // renders the tweets on the page a 3rd time to stay on the safe side (tested to be useful)
+      $("#error-element").empty().hide(); // Makes sure the error element is hidden after a successful tweet.
+      $('#tweet-text').val(""); // empties the tweet compose form
+      $("#charCount").val(140); // resets the charachter counter to 140
     }
   });
 
+  // loadTweets: makes an ajax GET call to the /tweets route,
+  // empties the tweet container,
+  // employs renderTweets function to display all tweets
   const loadTweets = function() {
     $.ajax({
       url: "/tweets",
@@ -80,26 +89,21 @@ $(document).ready(function() {
       }
     });
   };
-  loadTweets();
 
-  let mybutton = document.getElementById("myBtn");
+  loadTweets(); // The initial loadtweets function call to display all tweets at pageload
 
-  window.onscroll = function() {
-    scrollFunction();
-  };
-  // When the user scrolls down 20px from the top of the document, show the button
-  function scrollFunction() {
+  // When the user scrolls down 20px from the top of the document, show the scroll to top button
+  window.onscroll = function() { 
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       $("#myBtn").css("display", "block");
     } else {
       $("#myBtn").css("display", "none");
     }
-  }
-  // When the user clicks on the button, scroll to the top of the document
+  };
+  
+  // When the user clicks on the scroll to top button, scroll to the top of the document
   $("#myBtn").click(function() {
     $("html, body").animate({ scrollTop: "0" });
-    // document.body.scrollTop = 0;
-    // document.documentElement.scrollTop = 0;
   });
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
-  console.log("ready!");
+  
+  $('#tweet-text').on('input', (event) => { // activates on input event
+    const inputLength = event.target.value.length; // fetches the live input length
+    const actualLength = 140 - inputLength; // calculate remaining charachters till 140
+    const $counter = $('.counter'); // variable assignment to jQuery element
 
- 
-  $('#tweet-text').on('input', (event) => {
-    const inputLength = event.target.value.length;
-    const actualLength = 140 - inputLength;
-    const $counter = $('.counter');
-
-    $counter.text(actualLength);
+    $counter.text(actualLength); // assigns remaining charachters value
 
     if (actualLength < 0) {
-      $counter.css({ 'color': 'red'});
+      $counter.css({ 'color': 'red'}); // changes counter text color to red if charachters are over 140
     }
     if (actualLength > 0) {
-      $counter.css({ 'color': 'black'});
+      $counter.css({ 'color': 'black'}); // keeps/reverts back counter text color to black if charachters are under 140
     }
   });
 });
